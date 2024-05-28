decdcd6145f023f766a72edf06765ec04dabfaea
Ahhhhhhyeong <dkdud081@gmail.com>
2024-05-28 15:33:07 +0900

    테스트 코드 및 로그 xml 수정

--- max-point-diff/src/test/java/com/aykwon/peakdifference/maxpointdiff/MaxPointDiffApplicationTests.java
+++ max-point-diff/src/test/java/com/aykwon/peakdifference/maxpointdiff/MaxPointDiffApplicationTests.java
@@ -1,6 +1,5 @@
 package com.aykwon.peakdifference.maxpointdiff;
 
-import static org.mockito.Mockito.RETURNS_MOCKS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -21,13 +18,13 @@ class MaxPointDiffApplicationTests {
 	@Test
 	public void testControllerEndpoint() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/maxPointDiff")
-			.param("name", "model_data")
-			.contentType(MediaType.APPLICATION_JSON))
-			.andExpect(MockMvcResultMatchers.status().isOk());
+			.param("name", "model_ww3_data")
+			.contentType(MediaType.APPLICATION_JSON));
 	}
 
 	@Test
 	void contextLoads() {		
 	}
+
 
 }
